32054075bb001e5abf1929e4e75e65f9dca52405
afpineda <74291754+afpineda@users.noreply.github.com>
2025-11-13 11:20:42 +0100

    Fix send() and small style changes

--- src/NuS.cpp
+++ src/NuS.cpp
@@ -201,7 +201,8 @@ size_t NordicUARTService::write(const uint8_t *data, size_t size)
 
       while (remainingByteCount >= chunkSize)
       {
-         if (!pTxCharacteristic->notify(data, chunkSize)) {
+         if (!pTxCharacteristic->notify(data, chunkSize))
+         {
             // Notify failed - return how much we've sent so far
             return totalSent;
          }
@@ -210,13 +211,8 @@ size_t NordicUARTService::write(const uint8_t *data, size_t size)
          totalSent += chunkSize;
       }
       // Note: remainingByteCount < chunkSize at this point
-      if (remainingByteCount > 0) {
-         if (!pTxCharacteristic->notify(data, remainingByteCount)) {
-            // Notify failed - return how much we've sent so far
-            return totalSent;
-         }
+      if ((remainingByteCount > 0) && pTxCharacteristic->notify(data, remainingByteCount))
          totalSent += remainingByteCount;
-      }
 
       return totalSent;
    }
@@ -229,8 +225,7 @@ size_t NordicUARTService::send(const char *str, bool includeNullTerminatingChar)
    if (pTxCharacteristic)
    {
       size_t size = includeNullTerminatingChar ? strlen(str) + 1 : strlen(str);
-      write((uint8_t *)str, size);
-      return size;
+      return write((uint8_t *)str, size);
    }
    else
       return 0;
@@ -245,6 +240,7 @@ size_t NordicUARTService::printf(const char *format, ...)
    va_end(args);
    if (requiredSize == 0)
    {
+      // Write the terminating null character as we have an empty string
       return write((uint8_t *)&dummy, 1);
    }
    else if (requiredSize > 0)
