b06bbee3c476b99e032bdf5a066df880d9446070
afpineda <74291754+afpineda@users.noreply.github.com>
2025-12-11 12:22:49 +0100

    Enhance UartBleAdapter.ino

--- examples/README.md
+++ examples/README.md
@@ -85,9 +85,27 @@
 
 - [UartBleAdapter.ino](./UartBleAdapter/UartBleAdapter.ino)
 
-  Turns your ESP32 into UART to BLE adapter - transfers data between ESP32 TX/RX pins
-  and Bluetooth service. Also copies the same data data to USB UART interface,
-  and to UART1 on pins 4 and 5, which can be used to avoid seeing boot logs on UART0.
+  Turns your ESP32 into a **Bluetooth-to-UART bridge**.
+  Transfers data between the configured UARTs (TX/RX pins)
+  and the Nordic UART Service back and forth.
+
+  This is quite handy to debug code in other devices having a hardware UART,
+  but no USB-to-UART bridge.
+
+  Without modification, it bridges UART0, UART1 (configured to pins 4 and 5)
+  and the USB CDC UART (if available), so make sure the involved pins are not floating.
+  Otherwise, the sketch will read random data and send it to the Bluetooth side (⚠️).
+  You may edit the sketch to disable the unneeded UARTs.
+
+  > **Note**:
+  > if your device gets data from two or more UARTs,
+  > you don't know which one of them is sending it
+  > to the Bluetooth side.
+
+  If your devkit is connected to the PC serial monitor,
+  this sketch behaves quite similar to the *NuSEcho.ino* example above.
+  In such a case, make sure to configure
+  the same line ending character sequence in both sides.
 
 ## Testing
 
--- examples/UartBleAdapter/UartBleAdapter.ino
+++ examples/UartBleAdapter/UartBleAdapter.ino
@@ -1,10 +1,12 @@
 /**
  * @file UartBleAdapter.ino
  * @author Sergii Pylypenko.
+ *
  * @date 2025-12-10
- * @brief UART to BLE adapter - transfers data between ESP32 TX/RX pins
- *        and Bluetooth service. Also copies data to USB UART interface,
- *        and to UART1 on pins 4 and 5, because UART0 prints boot logs.
+ *
+ * @brief UART to BLE adapter - transfers data between
+ *        the configured ESP32's UARTS and the Nordic UART Service,
+ *        including hardware UARTS and the USB CDC UART (if available).
  *
  * @note See examples/README.md for a description
  *
@@ -12,100 +14,108 @@
  *
  */
 
-#include <Arduino.h>
+#include <HardwareSerial.h>
 #include "NuSerial.hpp"
 #include "NimBLEDevice.h"
 
-#define UART_SPEED 115200
+// Expected hardware UART baud rate for UART0
+// (using the default RX/TX pins) and
+// UART1 (configured to pins 4 and 5 below).
+// **Set to ZERO to ignore such an UART**.
+// All ESP32 chips are configured by default to 115200 bauds.
+// Make sure the device connected to the other endpoint
+// matches this baud rate.
+#define UART0_BAUD_RATE 115200
+#define UART1_BAUD_RATE 115200
 
-uint8_t rxBuf[512];
-size_t rxBufLen = 0;
-uint8_t txBuf[512];
-size_t txBufLen = 0;
-uint32_t lastTransferTime = 0;
+// NOTE:
+// To enable or disable the USB CDC UART (if available)
+// go to the "Board Configuration" option in Arduino IDE
+// and choose a value for "USB CDC on boot"
+
+// Buffer size (set as you wish)
+#define BUFFER_SIZE 2048
+// Read/write buffer to hold data in transit
+uint8_t data_buffer[BUFFER_SIZE];
 
 void setup()
 {
-    // Initialize serial monitor
+#if UART0_BAUD_RATE > 0
+    // Initialize the 1st hardware UART
     Serial0.setRxBufferSize(2048);
-    Serial0.begin(UART_SPEED);
+    Serial0.begin(UART0_BAUD_RATE);
     Serial0.setTimeout(20);
+#endif
+#if UART1_BAUD_RATE > 0
+    // Initialize the 2nd hardware UART
     Serial1.setRxBufferSize(2048);
-    Serial1.begin(UART_SPEED, SERIAL_8N1, 4, 5);
+    // Configured to pins 4 and 5. Feel free to change.
+    Serial1.begin(UART1_BAUD_RATE, SERIAL_8N1, 4, 5);
     Serial1.setTimeout(20);
+#endif
 #if ARDUINO_USB_CDC_ON_BOOT && ARDUINO_USB_MODE
+    // Initialize the USB CDC UART (if available)
     HWCDCSerial.setRxBufferSize(2048);
-    HWCDCSerial.begin(UART_SPEED);
+    HWCDCSerial.begin(); // Note: USB CDC ignores the baud parameter
     HWCDCSerial.setTimeout(20);
 #endif
 #if ARDUINO_USB_CDC_ON_BOOT && !ARDUINO_USB_MODE
     USBSerial.setRxBufferSize(2048);
-    USBSerial.begin(UART_SPEED);
+    USBSerial.begin(); // Note: USB CDC ignores the baud parameter
     USBSerial.setTimeout(20);
 #endif
 
     char name[17];
     snprintf(name, sizeof(name), "BLE-Serial-%04X", (uint16_t)ESP.getEfuseMac());
-    // Initialize BLE stack and Nordic UART service
+    // Initialize the BLE stack and the Nordic UART service
     NimBLEDevice::init(name);
     NimBLEDevice::getAdvertising()->setName(name);
-    NuSerial.begin(UART_SPEED);
+    NuSerial.begin(); // Note: NuS ignores the baud parameter
 }
+
+// Some general notes:
+// - We don't care about the connection state (there is no need to).
+// - We don't handle errors as there is no
+//   place to report them.
 
 void loop()
 {
-    if (!NuSerial.isConnected()) {
-        delay(10);
-        return;
-    }
+    // First, we read data from the configured UARTS
+    // and send it to NuSerial
+    // ---------------------------------------------
+    size_t available_size;
 
-    if (Serial0.available()) {
-        rxBuf[rxBufLen] = (uint8_t)Serial0.read();
-        rxBufLen ++;
-    } else if (Serial1.available()) {
-        rxBuf[rxBufLen] = (uint8_t)Serial1.read();
-        rxBufLen ++;
+#if UART0_BAUD_RATE > 0
+    available_size = Serial0.readBytes(data_buffer, Serial0.available());
+    NuSerial.write(data_buffer, available_size);
+#endif
+#if UART1_BAUD_RATE > 0
+    available_size = Serial1.readBytes(data_buffer, Serial1.available());
+    NuSerial.write(data_buffer, available_size);
+#endif
 #if ARDUINO_USB_CDC_ON_BOOT && ARDUINO_USB_MODE
-    } else if (HWCDCSerial.available()) {
-        rxBuf[rxBufLen] = (uint8_t)HWCDCSerial.read();
-        rxBufLen ++;
+    available_size = HWCDCSerial.readBytes(data_buffer, HWCDCSerial.available());
+    NuSerial.write(data_buffer, available_size);
 #endif
 #if ARDUINO_USB_CDC_ON_BOOT && !ARDUINO_USB_MODE
-    } else if (USBSerial.available()) {
-        rxBuf[rxBufLen] = (uint8_t)USBSerial.read();
-        rxBufLen ++;
+    available_size = USBSerial.readBytes(data_buffer, USBSerial.available());
+    NuSerial.write(data_buffer, available_size);
 #endif
-    } else {
-        delay(1);
-    }
 
-    if (rxBufLen >= sizeof(rxBuf) || rxBufLen > 0 && (
-            rxBuf[rxBufLen - 1] == '\n' ||
-            rxBuf[rxBufLen - 1] == '\r' ||
-            millis() > lastTransferTime + 100)) {
-        NuSerial.write(rxBuf, rxBufLen);
-        rxBufLen = 0;
-        lastTransferTime = millis();
-    }
-
-    while (NuSerial.available()) {
-        txBuf[txBufLen] = (uint8_t)NuSerial.read();
-        txBufLen ++;
-
-        if (txBufLen >= sizeof(txBuf) || txBufLen > 0 && (
-                txBuf[txBufLen - 1] == '\n' ||
-                txBuf[txBufLen - 1] == '\r' ||
-                millis() > lastTransferTime + 100)) {
-            Serial0.write(txBuf, txBufLen);
-            Serial1.write(txBuf, txBufLen);
+    // Next, we read data from NuSerial
+    // and send it to the configured UARTS
+    // ---------------------------------------------
+    available_size = NuSerial.readBytes(data_buffer, NuSerial.available());
+#if UART0_BAUD_RATE > 0
+    Serial0.write(data_buffer, available_size);
+#endif
+#if UART1_BAUD_RATE > 0
+    Serial1.write(data_buffer, available_size);
+#endif
 #if ARDUINO_USB_CDC_ON_BOOT && ARDUINO_USB_MODE
-            HWCDCSerial.write(txBuf, txBufLen);
+    HWCDCSerial.write(data_buffer, available_size);
 #endif
 #if ARDUINO_USB_CDC_ON_BOOT && !ARDUINO_USB_MODE
-            USBSerial.write(txBuf, txBufLen);
+    USBSerial.write(data_buffer, available_size);
 #endif
-            txBufLen = 0;
-            lastTransferTime = millis();
-        }
-    }
 }
